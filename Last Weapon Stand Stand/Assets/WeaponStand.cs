e3f95543ae2d105058c00230c5572cb1ce3c5200
Dragyn85 <nygard.jonas@gmail.com>
2024-05-31 22:27:38 +0200

    Added ammo box, ammo upgrade and healing upgrade

--- Last Weapon Stand Stand/Assets/WeaponStand.cs
+++ Last Weapon Stand Stand/Assets/WeaponStand.cs
@@ -2,19 +2,26 @@ using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class WeaponStand : MonoBehaviour
+public class WeaponStand : MonoBehaviour, IUpgrade
 {
+    [SerializeField] private int            maxHealth = 100;
     [SerializeField] private int            health     = 100;
     [SerializeField] private float          _alarmTime = 3.5f;
     [SerializeField] private Alarm          _alarm;
     [SerializeField] private StandHealthBar _standHealthBar;
 
+    [Header("Upgrade settings")]
+    [SerializeField] private int            repairAmount = 1;
+    [SerializeField] private float          repairTime = 10;
+    [SerializeField] private int            repairAmountIncrease =1;
+    [SerializeField] private float          repairTimeDecrease = 0.3f;
+    
     private float damageTimer;
-
+    private float nextRepairTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -27,9 +34,21 @@ public class WeaponStand : MonoBehaviour
             {
                 _alarm.AlarmOn = false;
             }
+            if(Time.time > nextRepairTime)
+            {
+                health += repairAmount;
+                health = Mathf.Clamp(health, 0, maxHealth);
+                nextRepairTime = Time.time + repairTime;
+            }
         }
     }
-
+    
+    public void Upgrade()
+    {
+        repairAmount += repairAmountIncrease;
+        repairTime -= repairTimeDecrease;
+    }
+    
     public void TakeDamage(int damage)
     {
         _alarm.AlarmOn         =  true;
