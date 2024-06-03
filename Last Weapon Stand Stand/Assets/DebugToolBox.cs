2326d4c17016dd22e3f897f40608877b4fc22823
David <djhatvr@gmail.com>
2024-06-03 14:32:55 -0700

    Fix Interaction Text

--- Last Weapon Stand Stand/Assets/DebugToolBox.cs
+++ Last Weapon Stand Stand/Assets/DebugToolBox.cs
@@ -10,11 +10,12 @@ public class DebugToolBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.K))
         {
             UnaliveAllAlieans();
         }
-        
+        */
     }
 
     private void UnaliveAllAlieans()
